3d81b92ac0333145d3324dc12940d3211a334b46
Darien <65925033+darmoment@users.noreply.github.com>
2022-09-23 18:30:13 -0400

    Parsed through all data field of csv

--- index.js
+++ index.js
@@ -5,5 +5,14 @@ async function getData() {
     const response = await fetch('NYPlants.csv');
     const data = await response.text();
     const rows = data.split('\n').slice(1);
-    console.log(rows);
+    rows.forEach(plant => {
+        const row = plant.split(',');
+        const scientificName = row[0];
+        const commonName = row[1];
+        const duration = row[2];
+        const habit = row[3];
+        const sun = row[4];
+        const water = row[5];
+        console.log(scientificName, commonName, duration, habit, sun, water);
+    })
 }
