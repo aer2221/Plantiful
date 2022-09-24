87bf9b39a97db7fefae44f6a63bbd5e653a82173
Anna Reis <anna@Annas-MacBook-Pro-8.local>
2022-09-24 10:15:25 -0400

    update

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
 
 async function getData() {
-    const response = await fetch('NYPlants.csv');
+    const response = await fetch('test.csv');
     const data = await response.text();
     const rows = data.split('\n').slice(1);
+    const dataArray = [];
+    
     rows.forEach(plant => {
         const row = plant.split(',');
-        const scientificName = row[0];
-        const commonName = row[1];
-        const duration = row[2];
-        const habit = row[3];
-        const sun = row[4];
-        const water = row[5];
-        console.log(scientificName, commonName, duration, habit, sun, water);
+        dataArray.push({scientificName: row[0], commonName: row[1], duration: row[2], habit: row[3], sun: row[4], water: row[5], image: row[6]});
     })
-}
+    const div = document.getElementById('darien');
+    div.innerHTML = "";
+    for(i = 0; i < 6; i++){
+      randInt = Math.floor(Math.random() * 14);
+      
+      var img = document.createElement('img');
+      img.src = dataArray[randInt].image;
+      div.appendChild(img);
+      scientificName = document.createTextNode("Scientific Name: " + dataArray[randInt].scientificName);
+      commonName = document.createTextNode("Common Name: " + dataArray[randInt].commonName);
+      duration = document.createTextNode("Life Duration: " + dataArray[randInt].duration);
+      habit = document.createTextNode("Type: " + dataArray[randInt].habit);
+      sun = document.createTextNode("How Much Sunlight: " + dataArray[randInt].sun);
+      water = document.createTextNode("How Much Water: " + dataArray[randInt].water);
+      div.innerHTML += "<br>";
+      div.appendChild(scientificName);
+      div.innerHTML += "<br>";
+      div.appendChild(commonName);
+      div.innerHTML += "<br>";
+      div.appendChild(duration);
+      div.innerHTML += "<br>";
+      div.appendChild(habit);
+      div.innerHTML += "<br>";
+      div.appendChild(sun);
+      div.innerHTML += "<br>";
+      div.appendChild(water);
+      div.innerHTML += "<br>";
+    };
+};
